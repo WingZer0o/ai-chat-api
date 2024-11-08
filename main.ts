efba5493da275ddcff9398564bf56b3471626f22
Mike Mulchrone <mtmulch0191@outlook.com>
2024-11-07 22:09:23 -0500

    basic structure for routes

--- main.ts
+++ main.ts
@@ -1,20 +1,8 @@
 import { Application } from "jsr:@oak/oak/application";
-import { Router } from "jsr:@oak/oak/router";
-
-const router = new Router();
-router.get("/", (ctx) => {
-  ctx.response.body = `<!DOCTYPE html>
-    <html>
-      <head><title>Hello oak!</title><head>
-      <body>
-        <h1>Hello oak!</h1>
-      </body>
-    </html>
-  `;
-});
+import IndexRouter from "./routes/index-router.ts";
 
 const app = new Application();
-app.use(router.routes());
-app.use(router.allowedMethods());
+app.use(IndexRouter.routes());
+app.use(IndexRouter.allowedMethods());
 
 app.listen({ port: 8080 });
