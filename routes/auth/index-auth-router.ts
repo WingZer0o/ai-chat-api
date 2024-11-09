7b6b8a18e4c14c54f302986480c5dc801cd28275
WingZer0o <mikemulchrone987@gmail.com>
2024-11-08 19:23:31 -0500

    basic argon2 hashing import with JSR

--- routes/auth/index-auth-router.ts
+++ routes/auth/index-auth-router.ts
@@ -1,9 +1,11 @@
+import * as felixArgon from "@felix/argon2";
 import { Router } from "@oak/oak/router";
 
 const router = new Router();
 
-router.get("/test", (ctx) => {
-    ctx.response.body = "Hello World!";
+router.post("/test", async (ctx) => {
+    const body = await ctx.request.body.json();
+    ctx.response.body = await felixArgon.hash(body.welcome);
 });
 
 export default router;
