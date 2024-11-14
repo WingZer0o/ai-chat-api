bfcc2b62716137413cf8aa230a475ea866ffc0b9
WingZer0o <mikemulchrone987@gmail.com>
2024-11-13 21:55:31 -0500

    basic making jwt and verifying in the same route

---
+++ models/auth/get-token-dto.ts
@@ -0,0 +1,4 @@
+export class GetTokenDto {
+    public email!: string;
+    public password!: string;
+}
---
+++ routes/auth/token-router.ts
@@ -0,0 +1,46 @@
+import { Router } from "@oak/oak/router";
+import { Argon2Wrapper, RSAWrapper } from "cas-typescript-sdk";
+import { eq } from "drizzle-orm/expressions";
+import jwt from "jsonwebtoken";
+import { db, USERS } from "../../db/schema.ts";
+import { GetTokenDto } from "../../models/auth/get-token-dto.ts";
+import { UserDoesntExistDto } from "../../models/auth/user-doesnt-exist-dto.ts";
+
+const router = new Router();
+
+router.post("/token", async (ctx) => {
+    try {
+        const body: GetTokenDto = await ctx.request.body.json();
+        // TODO: email regex
+        const doesUserExist = await db.select().from(USERS).where(
+            eq(USERS.email, body.email),
+        );
+        if (doesUserExist.length === 0) {
+            ctx.response.status = 400;
+            ctx.response.body = new UserDoesntExistDto("User doesn't exist");
+            return;
+        }
+        const isPasswordValid = new Argon2Wrapper().verify(
+            doesUserExist[0].password!,
+            body.password,
+        );
+        if (!isPasswordValid) {
+            ctx.response.status = 401;
+            return;
+        }
+        const rsa = new RSAWrapper();
+        const rsaKeys = rsa.generateKeys(4096);
+        var token = jwt.sign({ foo: "bar" }, rsaKeys.privateKey, {
+            algorithm: "RS256",
+        });
+        await jwt.verify(
+            token,
+            rsaKeys.publicKey,
+        );
+    } catch (error) {
+        ctx.response.status = 500;
+        return;
+    }
+});
+
+export default router;
