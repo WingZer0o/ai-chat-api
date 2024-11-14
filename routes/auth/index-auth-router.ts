361d220df9665329741c52ac3611f69ee0039ad2
WingZer0o <mikemulchrone987@gmail.com>
2024-11-13 21:17:10 -0500

    query for existing user

--- routes/auth/index-auth-router.ts
+++ routes/auth/index-auth-router.ts
@@ -1,7 +1,10 @@
 import { Router } from "@oak/oak/router";
+import { eq } from "drizzle-orm/expressions";
 import { UserDBO } from "../../db/auth/user-dbo.ts";
 import { db, USERS } from "../../db/schema.ts";
 import { CreateUserDto } from "../../models/auth/create-user-dto.ts";
+import { UserCreatedDto } from "../../models/auth/user-created-dto.ts";
+import { UserExistsDto } from "../../models/auth/user-exists-dto.ts";
 
 const router = new Router();
 
@@ -10,8 +13,17 @@ router.post("/create-user", async (ctx) => {
     const userDBO = new UserDBO();
     userDBO.email = body.email;
     userDBO.password = body.password;
+    const doesUserExist = await db.select().from(USERS).where(
+        eq(USERS.email, userDBO.email),
+    );
+    if (doesUserExist.length > 0) {
+        ctx.response.status = 400;
+        ctx.response.body = new UserExistsDto("User already exists");
+        return;
+    }
     await db.insert(USERS).values(userDBO);
-    ctx.response.body = userDBO;
+    ctx.response.status = 200;
+    ctx.response.body = new UserCreatedDto("User created");
 });
 
 export default router;
