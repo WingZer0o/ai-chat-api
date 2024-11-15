6c2559dc5490b4184446b12c1fd0f5d1763c5cdc
WingZer0o <mikemulchrone987@gmail.com>
2024-11-14 21:01:04 -0500

    basic testing with ollama

--- main.ts
+++ main.ts
@@ -5,6 +5,5 @@ import IndexRouter from "./routes/index-router.ts";
 const app = new Application();
 app.use(oakCors({ origin: "http://localhost:4200" }));
 app.use(IndexRouter.routes());
-app.use(IndexRouter.allowedMethods());
 
 app.listen({ port: 8080 });
--- routes/auth/token-router.ts
+++ routes/auth/token-router.ts
@@ -1,3 +1,4 @@
+import { Ollama } from "@langchain/ollama";
 import { Router } from "@oak/oak/router";
 import { Argon2Wrapper, RSAWrapper } from "cas-typescript-sdk";
 import { eq } from "drizzle-orm/expressions";
@@ -35,11 +36,22 @@ router.post("/token", async (ctx) => {
         const token = jwt.sign({ userId: user[0].id }, rsaKeys.privateKey, {
             algorithm: "RS256",
         });
-        // TODO: store public key in redis cache
+        const llm = new Ollama({
+            model: "llama3.1",
+            baseUrl: "http://host.docker.internal:11434", // Default value
+            temperature: 0,
+            maxRetries: 2,
+            // other params...
+        });
+        const inputText = "Ollama is an AI company that ";
+
+        const completion = await llm.invoke(inputText);
+        console.log(completion);
         ctx.response.status = 200;
         ctx.response.body = new GetTokenResponseDto(token);
     } catch (error) {
         ctx.response.status = 500;
+        console.log(error);
         return;
     }
 });
